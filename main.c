79b0a959d377ddfa8e5c754486ed2caf0ff50b00
Jack Pattison <pattisonj205@gmail.com>
2019-04-29 06:50:00 +0000

    Cannot work in Che. Worked in GBD Online.

--- main.c
+++ main.c
@@ -7,29 +7,29 @@ int main()
     printf("b) Rotation Decryption with Key Input\n");
     printf("Selection: ");
     char menu;
-    scanf("%c", &menu);
+    scanf("%c", &menu);//reads user's letter selection for menu array
     switch(menu) 
     {
-        case 'a':
+        case 'a': 
         {
-            int i, key;
-            char string[i];
+            int i, key;//key will be a user input. i will be a counter.
+            char string[100]; //string with size 100. This is a limitation.
             printf("Message: ");
-            scanf("%s", string);
+            scanf("%s", string); //
             printf("Key: ");
             scanf("%d", &key);
-            for(i = 0; (i < 100 && string[i] != '\0'); i++)
-            if(string[i] <= 90 && string[i] >= 65)
+            for(i = 0; (i < 100 && string[i] != '\0'); i++) //for statement establishes size of string.
+            if(string[i] <= 90 && string[i] >= 65) //ensures only characters between A-Z are altered.
             {
-                string[i] = string[i] + key;
-            if(string[i] > 90)
+                string[i] = string[i] + key;//shifts ACSII Characters are shifted by key and stored back into the string.
+            if(string[i] > 90)//Statements handles inputs over Z.
             {
-                string[i] = string[i] - 26;
+                string[i] = string[i] - 26;//shifts chacters back into range.
             }
             }
-            else if(string[i] <= 122 && string[i] >= 97 )
+            else if(string[i] <= 122 && string[i] >= 97 ) //statements covers lower case letters
             {
-                string[i] = string[i] - 32;
+                string[i] = string[i] - 32; //shifts characters into capital letter range.
                 string[i] = string[i] + key;
             if(string[i] > 90)
             {
